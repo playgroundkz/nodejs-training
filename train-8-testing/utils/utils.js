a87557516943a3d1992b8ecf384a2dcac1710395
Таскинбаев Б. И <taskibaev.ab@gmail.com>
2018-06-06 08:36:28 +0500

    testing application help assertions expect lib

--- train-8-testing/utils/utils.js
+++ train-8-testing/utils/utils.js
@@ -1,1 +1,9 @@
-module.exports.add = (a, b)=> a+b+22
+module.exports.add = (a, b)=> a+b
+module.exports.minus = (a, b)=> a-b
+
+module.exports.setUserName = (user, fullName) => {
+    var names = fullName.split(" ")
+    user.firstName = names[0]
+    user.lastName = names[1]
+    return user
+}
--- train-8-testing/utils/utils.test.js
+++ train-8-testing/utils/utils.test.js
@@ -1,7 +1,36 @@
+const expect = require('expect')
+
 const utils = require('./utils')
+
 it('should plus two numbers', () => {
     var res = utils.add(33,11)
     if (res != 44){
         throw Error(`Expected 44, but we get ${res}`)
     }
+})
+
+it('should minus two numbers', () => {
+    var res = utils.minus(33,11)
+    expect(res).toBe(22)
+    // if (res != 22){
+    //     throw Error(`Expected 22, but we get ${res}`)
+    // }
+})
+
+it ('it should verify names', ()=>{
+    var object = {
+        firstName : '',
+        lastName : '',
+        age : 25,
+    }
+    var firstName = "Abdurakhman"
+    var lastName = "Taskibaev"
+    var fullName = `${firstName} ${lastName}`
+
+    var resObject = utils.setUserName(object, fullName)
+
+    expect(resObject).toInclude({
+        firstName : firstName,
+        lastName : lastName,
+    })
 })

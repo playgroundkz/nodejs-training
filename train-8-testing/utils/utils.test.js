bd521cf53c67fb61e5cf84c620e1da5eb9a9b0f8
unknown <taskibaev.ab@gmail.com>
2018-06-12 13:12:46 +0500

    some changes on work station

--- train-8-testing/utils/utils.test.js
+++ train-8-testing/utils/utils.test.js
@@ -2,26 +2,29 @@ const expect = require('expect')
 
 const utils = require('./utils')
 
-it('should plus two numbers', () => {
-    var res = utils.add(33,11)
-    if (res != 44){
-        throw Error(`Expected 44, but we get ${res}`)
-    }
-})
-
-it("it should async add two numbers", (done)=>{
-    utils.asyncAdd(3,4,(sum)=>{
-        expect(sum).toBe(7)
-        done()
+describe("Users", ()=>{
+    
+    it('should plus two numbers', () => {
+        var res = utils.add(33,11)
+        if (res != 44){
+            throw Error(`Expected 44, but we get ${res}`)
+        }
     })
-})
 
-it('should minus two numbers', () => {
-    var res = utils.minus(33,11)
-    expect(res).toBe(22)
-    // if (res != 22){
-    //     throw Error(`Expected 22, but we get ${res}`)
-    // }
+    it("it should async add two numbers", (done)=>{
+        utils.asyncAdd(3,4,(sum)=>{
+            expect(sum).toBe(7)
+            done()
+        })
+    })
+
+    it('should minus two numbers', () => {
+        var res = utils.minus(33,11)
+        expect(res).toBe(22)
+        // if (res != 22){
+        //     throw Error(`Expected 22, but we get ${res}`)
+        // }
+    })
 })
 
 it ('it should verify names', ()=>{

6933ce6db83a1f6ebf4365288de7ccaea20f65e9
unknown <taskibaev.ab@gmail.com>
2018-06-13 13:58:36 +0500

    mongoose getting started

---
+++ train-9-mongo/server/server.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+
+mongoose.connect("mongodb://localhost:27017/TodoApp")
+
+var Todo = mongoose.model("Todo", {
+    text : {
+        type:String,
+        required: true,
+        minlenght:1,
+        trim:true,
+    },
+    completed : {
+      type:Boolean,
+      default: false,
+    },
+    completedAt : {
+        type:Number,
+        default:null
+    }       
+})
+
+var newTodo = new Todo({
+    text:"Walk a dog"
+});
+
+newTodo.save().then(
+    (result) => {
+        console.log("New model was successfully added to database")
+        console.log(result)
+    },
+   (err) => {
+    console.log("Something went wrong! ", err)
+})
